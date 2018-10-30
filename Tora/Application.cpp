b4efef99fced3495bac2dc1c3043a01025c0b3e9
Trevor <16tsmith55@gmail.com>
2018-10-30 00:59:50 -0600

    added working buffer but not quite draw triangle yet

--- Tora/Application.cpp
+++ Tora/Application.cpp
@@ -1,5 +1,6 @@
+#include <GL/glew.h>
 #include <glfw3.h>
-
+#include <iostream>
 int main(void)
 {
 	GLFWwindow* window;
@@ -7,6 +8,7 @@ int main(void)
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
+
 	
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -19,17 +21,29 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+
+	if (glewInit() != GLEW_OK)
+		std::cout << "ERROR" << std::endl;
+	std::cout <<"Version : "<<glGetString(GL_VERSION) << std::endl;
+
+	float positions[6] = {
+		-0.5f, -0.5f,
+		0.0f, 0.5f,
+		0.5f, -0.5f
+	};
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
